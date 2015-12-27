878c7eb9e58813df00238e2da0834c148a93ba6b
leon.shi <superRaytin@163.com>
2015-12-28 02:24:15 +0800

    update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,19 @@
-﻿var gulp = require('gulp');
+﻿var path = require('path');
+var fs = require('fs');
+var gulp = require('gulp');
+var through2 = require("through2");
 var uglify = require('gulp-uglify');
 var browserify = require('gulp-browserify');
 var rename = require("gulp-rename");
+var ignore = require("gulp-ignore");
 var banner = require("gulp-banner");
+var marked = require("gulp-marked");
+var lodashTemplate = require("lodash.template");
+var shelljs = require('shelljs');
+var ghPages = require("gh-pages");
 var pkg = require('./package.json');
+
+var cwd = process.cwd();
 
 var comment = '/*\n' +
     ' * <%= pkg.name %> <%= pkg.version %>\n' +
@@ -14,10 +24,74 @@ var comment = '/*\n' +
     ' * Released under the <%= pkg.license %> license.\n' +
     '*/\n\n';
 
-gulp.task('build', function() {
+function getHtmlFile(dir) {
+    var exampleDir = path.join(cwd, dir);
+    var files = fs.readdirSync(exampleDir);
+    var entry = [];
+
+    files.forEach(function(file) {
+        var extname = path.extname(file);
+        var name = path.basename(file, extname);
+        if (extname === '.html') {
+            entry.push(name);
+        }
+    });
+
+    return entry;
+}
+
+gulp.task('default', function() {
+    gulp.start(['browserify', 'gh-pages']);
+});
+
+gulp.task('cleanBuild', function() {
+    shelljs.rm('-rf', path.join(cwd, 'build'));
+});
+
+gulp.task('before-gh-pages', function() {
+    gulp.start(['copy-example', 'example-browserify', 'readme']);
+});
+
+gulp.task('copy-example', function() {
+    gulp.src('example/**/*')
+        .pipe(ignore.exclude('*.js'))
+        .pipe(ignore.exclude('template.html'))
+        .pipe(gulp.dest('build/example'));
+});
+
+gulp.task('readme', function() {
+    gulp.src('./README.md')
+        .pipe(marked({
+
+        }))
+        .pipe(through2.obj(function(file, encoding, next) {
+            var that = this;
+            var content = file.contents.toString(encoding);
+
+            fs.readFile('./example/template.html', function(err, data) {
+                if (err) throw err;
+                var parseData = data.toString();
+                var temp = lodashTemplate(parseData);
+
+                content = temp({
+                    pkg: pkg,
+                    examples: getHtmlFile('./build/example'),
+                    body: content
+                });
+
+                file.contents = new Buffer(content, encoding);
+                that.push(file);
+                next();
+            });
+        }))
+        .pipe(rename('index.html'))
+        .pipe(gulp.dest('build'));
+});
+
+gulp.task('browserify', function() {
     gulp.src('lib/index.js')
         .pipe(browserify({
-            ignore: ['fs', 'buffer']
+            ignore: ['fs', 'Buffer']
         }))
         .pipe(uglify())
         .pipe(banner(comment, {
@@ -25,4 +99,41 @@ gulp.task('build', function() {
         }))
         .pipe(rename('image-to-slices.js'))
         .pipe(gulp.dest('dist'));
+});
+
+gulp.task('example-browserify', function() {
+    var exampleComment = '/*\n' +
+        ' * <%= pkg.name %>@<%= pkg.version %> example\n' +
+        ' * <%= pkg.homepage %>\n' +
+        ' *\n' +
+        ' * Copyright 2015, <%= pkg.author %>\n' +
+        '*/\n\n';
+
+    gulp.src('example/*.js')
+        .pipe(browserify({
+            ignore: ['fs', 'Buffer']
+        }))
+        .pipe(uglify())
+        .pipe(banner(exampleComment, {
+            pkg: pkg
+        }))
+        .pipe(gulp.dest('build/example'));
+});
+
+gulp.task('gh-pages', ['before-gh-pages'], function(done) {
+    console.log('gh-pages start');
+    console.log(cwd);
+
+    if (fs.existsSync(path.join(cwd, './example/'))) {
+        ghPages.publish(path.join(cwd, 'build'), {
+            logger: function(message) {
+                console.log(message);
+            }
+        }, function() {
+            console.log('gh-pages end');
+            done();
+        });
+    } else {
+        done();
+    }
 });
